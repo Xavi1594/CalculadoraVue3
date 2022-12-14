e3cf806af9a5a9445fc41a3133dd5e2413e1ab86
GabriMF <108740750+GabriMF@users.noreply.github.com>
2022-12-14 10:52:32 +0100

    sincronizando carpetas

---
+++ src/components/home/calculatorView.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <section id="calculator">
+
+    </section>
+</template>
+
+<style scoped>
+#calculator{
+    width: 80%;
+    height: 60%;
+    background-color: red;
+}
+</style>
